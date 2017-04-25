75b036ca75df0f3e89f2b7a815e129c557d638d0
Anton <a.kiyatkin@itlf.ru>
2017-04-25 18:32:59 +0400

    dealers

--- infra.php
+++ infra.php
@@ -2,15 +2,28 @@
 use infrajs\event\Event;
 use infrajs\view\View;
 use infrajs\load\Load;
+use infrajs\access\Access;
+use infrajs\once\Once;
+use MatthiasMullie\Minify;
 use infrajs\config\Config;
 
 Event::one('Layer.onshow', function (&$layer) {
 	if (empty($layer['checkversion'])) return; //Контроль версий не включён
-	$data = Config::pub('checkversion');
-	$data['time'] = time();
-	$data = Load::json_encode($data);
-	$html = '<script>window.checkversion='.$data.';';
-	$html .= Load::loadTEXT('-checkversion/check.js');
-	$html .= '</script>';
-	View::head($html, true);
-});
+
+	$html = Access::cache(__FILE__, function () {
+		$data = Config::pub('checkversion');
+		$data['time'] = time();
+		$data = Load::json_encode($data);
+		$html = '<script>';
+		$code = 'window.checkversion='.$data.';';
+		$code .= Load::loadTEXT('-checkversion/check.js');
+		$min = new Minify\JS($code);
+		$code = $min->minify();
+		$html .= $code;
+		$html .= '</script>';
+		return $html;
+	});
+	Once::exec(__FILE__, function () use ($html) {
+		View::head($html, true);
+	});
+}, 'checkversion:tpl');
